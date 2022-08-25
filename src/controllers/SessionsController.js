c78e5e911dfa97f00e515a1db0cc9d7692f50d8c
Alex <alexxcamargo1000@gmail.com>
2022-08-25 10:41:20 -0300

    feat: add  user token

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -1,23 +1,34 @@
 const knex = require("../database/knex");
 const AppError = require("../utils/AppError");
-const { compare } = require('bcryptjs')
+const { compare } = require("bcryptjs");
+const authConfig = require("../configs/auth");
+const { sign } = require("jsonwebtoken");
+
+
 class SessionsController {
   async create(request, response) {
     const { email, password } = request.body;
 
     const user = await knex("users").where({ email }).first();
- 
+
     if (!user) {
-      throw new AppError("Email e/ou senha incorreta",401 );
+      throw new AppError("Email e/ou senha incorreta", 401);
     }
-    
+
     const isPasswordChecked = await compare(password, user.password);
 
-    if(!isPasswordChecked) {
-      throw new AppError("Email e/ou senha incorreta",401 );
+    if (!isPasswordChecked) {
+      throw new AppError("Email e/ou senha incorreta", 401);
     }
 
-    return response.json(user);
+    const {secret, expiresIn} = authConfig.jwt;
+
+    const token = sign({}, secret,{
+      subject: String(user.id),
+      expiresIn
+    })
+
+    return response.json({user, token});
   }
 }
 
